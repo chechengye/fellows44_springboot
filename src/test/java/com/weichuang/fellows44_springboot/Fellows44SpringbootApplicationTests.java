61b7fd6af82b2ce709077fe3d6d99c3cbd71834f
chechengye <262926400@qq.com>
2021-01-22 16:34:24 +0800

    多属性文件与日志

--- src/test/java/com/weichuang/fellows44_springboot/Fellows44SpringbootApplicationTests.java
+++ src/test/java/com/weichuang/fellows44_springboot/Fellows44SpringbootApplicationTests.java
@@ -2,17 +2,31 @@ package com.weichuang.fellows44_springboot;
 
 import com.weichuang.fellows44_springboot.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Fellows44SpringbootApplicationTests {
 
+	Logger logger = LoggerFactory.getLogger(getClass());
 	@Autowired
 	Person person;
 	@Test
 	void contextLoads() {
 		System.out.println("person = " + person);
+	}
+
+	//日志级别：跟踪日志<调试日志<打印日志<警告日志<错误日志
+	@Test
+	void loggerTest(){
+		System.out.println("测试日志输出");
+		logger.trace("这是跟踪日志");
+		logger.debug("这是调试日志");
+		logger.info("这是打印日志");
+		logger.warn("这是警告日志");
+		logger.error("这是一个错误日志");
 	}
 
 }
